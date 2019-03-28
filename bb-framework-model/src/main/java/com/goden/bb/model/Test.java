13fb587be6ed2fc5ae8acc8b785807c73a5674e4
alice <alice@163.com>
2019-03-28 15:45:14 +0800

    测试

---
+++ bb-framework-model/src/main/java/com/goden/bb/model/Test.java
@@ -0,0 +1,18 @@
+package com.goden.bb.model;
+
+import javax.sound.midi.Soundbank;
+
+/**
+ * @program: BusinessServer
+ * @description: 测试
+ * @author: Goden
+ * @create: 2019-03-28 15:41
+ **/
+public class Test {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        for (int i = 0; i < 10; i++) {
+            System.out.println(s);
+        }
+    }
+}
